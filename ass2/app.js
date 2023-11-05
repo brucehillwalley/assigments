8b4be3570a886af0dac9ca0a2fdf5aad6f7525b4
BruceHill <brucehillwalley@gmail.com>
2023-11-05 23:14:11 +0300

    rastgele arkaplan rengi yapıldı

--- ass2/app.js
+++ ass2/app.js
@@ -1,1 +1,21 @@
-// Kodlar buraya
+const bg = document.querySelector(".bgDiv");
+
+// bg.style.backgroundColor="red"
+
+const btnClick = document.querySelector(".btn-click");
+const btnMouseOver = document.querySelector(".btn-over");
+
+// btnClick.style.backgroundColor = "red";
+
+function renkUret() {
+  // return Math.floor(Math.random()*256)
+
+  return `rgb(${Math.floor(Math.random() * 256)},${Math.floor(
+    Math.random() * 256
+  )},${Math.floor(Math.random() * 256)})`;
+}
+
+btnClick.addEventListener("click", (e) => {
+  // bg.style.backgroundColor=`rgb(${renkUret()},${renkUret()},${renkUret()})`
+  bg.style.backgroundColor = renkUret();
+});
